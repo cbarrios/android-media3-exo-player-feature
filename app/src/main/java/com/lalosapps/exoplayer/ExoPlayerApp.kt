5d014b7e67f01e4dff28393198444a28dfd6061b
cbarrios <lalobarrios16@yahoo.com>
2022-10-12 12:21:15 -0400

    master: (3) Ui done + Hilt module

---
+++ app/src/main/java/com/lalosapps/exoplayer/ExoPlayerApp.kt
@@ -0,0 +1,7 @@
+package com.lalosapps.exoplayer
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ExoPlayerApp: Application()
--- app/src/main/java/com/lalosapps/exoplayer/MainActivity.kt
+++ app/src/main/java/com/lalosapps/exoplayer/MainActivity.kt
@@ -2,13 +2,29 @@ package com.lalosapps.exoplayer
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FileOpen
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.media3.ui.PlayerView
 import com.lalosapps.exoplayer.ui.theme.ExoPlayerTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +35,76 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    val viewModel: MainViewModel = hiltViewModel()
+                    val videoItems by viewModel.videoItems.collectAsState()
+                    val selectVideoLauncher = rememberLauncherForActivityResult(
+                        contract = ActivityResultContracts.GetContent(),
+                        onResult = { uri ->
+                            uri?.let(block = viewModel::addVideoUri)
+                        }
+                    )
 
+                    var lifecycle by remember {
+                        mutableStateOf(Lifecycle.Event.ON_CREATE)
+                    }
+                    val lifecycleOwner = LocalLifecycleOwner.current
+                    DisposableEffect(key1 = lifecycleOwner) {
+                        val observer = LifecycleEventObserver { _, event -> lifecycle = event }
+                        lifecycleOwner.lifecycle.addObserver(observer)
+                        onDispose {
+                            lifecycleOwner.lifecycle.removeObserver(observer)
+                        }
+                    }
+
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp)
+                    ) {
+                        AndroidView(
+                            factory = { ctx ->
+                                PlayerView(ctx).also {
+                                    it.player = viewModel.player
+                                }
+                            },
+                            update = {
+                                when (lifecycle) {
+                                    Lifecycle.Event.ON_PAUSE -> {
+                                        it.onPause()
+                                        it.player?.pause()
+                                    }
+                                    Lifecycle.Event.ON_RESUME -> {
+                                        it.onResume()
+                                    }
+                                    else -> Unit
+                                }
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .aspectRatio(16 / 9f)
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        IconButton(onClick = { selectVideoLauncher.launch("video/mp4") }) {
+                            Icon(
+                                imageVector = Icons.Default.FileOpen,
+                                contentDescription = "Select video"
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(16.dp))
+                        LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                            items(videoItems) { item ->
+                                Text(
+                                    text = item.name,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .clickable {
+                                            viewModel.playVideo(item.contentUri)
+                                        }
+                                        .padding(16.dp)
+                                )
+                            }
+                        }
+                    }
                 }
             }
         }
